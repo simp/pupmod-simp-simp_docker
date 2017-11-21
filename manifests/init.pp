3683088a1947bf3e8e68e204c0b06068b0e90bc7
Nick Miller <nick.miller@onyxpoint.com>
2017-11-21 16:52:42 -0500

    smarter toggle for iptables

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,13 +3,13 @@ class simp_docker (
   Simp_docker::Type $release_type,
   Boolean $manage_sysctl,
   String $docker_bridge_dev,
-  Boolean $iptables_hack,
 
   Hash $default_options,
   Optional[Hash] $other_options,
+
+  Boolean $iptables_hack = simplib::lookup('simp_options::firewall', { 'default_value' => true }),
 ) {
 
-  # include "simp_docker::${type}"
   $_docker_bridge_up = ($docker_bridge_dev in $facts['networking']['interfaces'].keys)
   if $manage_sysctl and $_docker_bridge_up {
     sysctl {
